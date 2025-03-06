267171410e94a2ca7f818b62e51396bb3a7ef7b2
Scorpionweb731 <patelweb6022@gmail.com>
2025-03-06 14:43:17 +0530

    product section generated

---
+++ Data.js
@@ -0,0 +1,61 @@
+ const products = [
+
+    {
+        imagepath : './images/products/resized/pedigree_food.png',
+        issale: true,
+        name : 'Pedigree' ,
+        rating: 5,
+        price : 300        
+    },
+    {
+        imagepath : './images/products/resized/wishkas.png',
+        issale: true,
+        name : 'Wishkas' ,
+        rating: 3,
+        price : 200        
+    },
+    {
+        imagepath : './images/products/resized/pedigree_food.png',
+        issale: true,
+        name : 'Pedigree' ,
+        rating: 5,
+        price : 300        
+    },
+    {
+        imagepath : './images/products/resized/wishkas.png',
+        issale: true,
+        name : 'Wishkas' ,
+        rating: 4,
+        price : 200        
+    },
+    {
+        imagepath : './images/products/resized/pedigree_food.png',
+        issale: true,
+        name : 'Pedigree' ,
+        rating: 2,
+        price : 300        
+    },
+    {
+        imagepath : './images/products/resized/wishkas.png',
+        issale: true,
+        name : 'Wishkas' ,
+        rating: 5,
+        price : 200        
+    },
+    {
+        imagepath : './images/products/resized/pedigree_food.png',
+        issale: true,
+        name : 'Pedigree' ,
+        rating: 3,
+        price : 300        
+    },
+    {
+        imagepath : './images/products/resized/wishkas.png',
+        issale: true,
+        name : 'Wishkas' ,
+        rating: 4,
+        price : 200        
+    },
+]
+
+export default products
